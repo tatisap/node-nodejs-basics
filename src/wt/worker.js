c90fbe5ea47c1eae96f168f714e8c6bcbb524074
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-12-04 18:42:19 +0300

    feat: implement wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,23 @@
-// n should be received from main thread
+import { fileURLToPath } from 'url';
+import { Worker, isMainThread, parentPort } from 'worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  const filename = fileURLToPath(import.meta.url);
+  if (isMainThread) {
+    const worker = new Worker(filename);
+    worker.once('message', (value) => {
+      console.log(value);
+    });
+    //Send Fubonacci number position (4) to the worker
+    worker.postMessage(4);
+  } else {
+    parentPort.once('message', (value) => {
+      const result = nthFibonacci(value);
+      parentPort.postMessage(result);
+    });
+  }
 };
 
 sendResult();
