3cb4e1f8fc757414fcaecd92c3f7463b63d8d95a
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-12-04 18:04:54 +0300

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const stream = createReadStream(path.join(dirname, 'files', 'fileToRead.txt'), 'utf-8');
+
+  let content = '';
+  stream.on('data', (chunk) => content += chunk);
+  stream.on('end', () => stdout.write(`${content}\n`));
+  stream.on('error', (err) => console.error(err.message));
 };
 
 await read();
