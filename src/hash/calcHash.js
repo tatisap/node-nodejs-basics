e762a48ec61467917bece20f1f0d59a5c4226c81
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-12-04 17:59:42 +0300

    feat: implement calsHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const content = await readFile(path.join(dirname, 'files', 'fileToCalculateHashFor.txt'), { encoding: 'utf-8' });
+  const { createHash } = await import('node:crypto');
+
+  const hash = createHash('sha256');
+  hash.update(content);
+  console.log(hash.digest('hex'));
 };
 
 await calculateHash();
