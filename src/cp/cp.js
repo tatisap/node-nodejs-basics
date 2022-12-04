91985f618b5fc4a74b8cb9e34ee4e2b7a6f4f60e
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-12-04 20:40:27 +0300

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const spawnChildProcess = async (args) => {
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const scriptPath = path.join(dirname, 'files', 'script.js');
+  const node = spawn('node', [scriptPath, ...args], { stdio: [0, 1, 2, 'ipc'] });
+  node.on('error', (err) => console.error(err.message));
+};
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3]);
