e77a5ac3c90ec4f9c9b0a7994181becbeadb8cd3
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-12-04 17:06:27 +0300

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { unlink } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const removePath = path.join(dirname, 'files', 'fileToRemove.txt');
+  try {
+    await unlink(removePath);
+  }
+  catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  }
 };
 
 await remove();
