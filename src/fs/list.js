b2ac3cd547bebfdc6de21a81a4101219ac244f8d
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-12-04 17:17:53 +0300

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { readdir } from 'fs/promises';
+
 const list = async () => {
-    // Write your code here 
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const folderPath = path.join(dirname, 'files');
+  try {
+    const files = await readdir(folderPath, { withFileTypes: true });
+    for await (const file of files) {
+      if (file.isFile()) {
+        console.log(file.name);
+      }
+    }
+  }
+  catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  } 
 };
 
 await list();
