46d321c0ba04641acd660b37a71681430bb3ef41
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-12-04 17:23:33 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(dirname, 'files', 'fileToRead.txt');
+  try {
+    const content = await readFile(filePath, { encoding: 'utf-8' });
+    console.log(content);
+  }
+  catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  }
 };
 
 await read();
